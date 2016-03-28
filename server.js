4b4c7d14a63fcf9d66b2002519fca754d50f9041
seforarodriguez <seforarodriguez13@gmail.com>
2016-03-28 15:59:14 -0500

    commit of database base structure

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const express = require('express')
   ,   app = express()
   ,   mongoose = require('mongoose')
   ,   PORT = process.env.PORT || 3000
+  ,   routes = require('./routes/routes')
 
-
+app.use(routes);
 mongoose.connect('mongodb://localhost:27017/nodedatabase', (err) => {
   if (err) throw err;
 
